000f0587a9835f2a6d49b256053b5ab55e2eda90
mitchellhart <mitchellhart@gmail.com>
2024-12-05 18:03:44 -0800

    adds root level config files

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT || 5431,
+  port: process.env.POSTGRES_PORT || 5432,
 });
 
 // Define authentication middleware
